aa6fa860cab34fa536b1ade275e31b75ade29055
AlbgoncebDAM <agonzalezc2502@educantabria.es>
2026-02-05 17:38:42 +0100

    atributos mapa

---
+++ JavaLand/src/main/java/tema6/javaland/Mapa.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tema6.javaland;
+
+/**
+ *
+ * @author DAM126
+ */
+public class Mapa {
+    private String MapaInterface[][] = new String[10][10];
+    private final String valientes = "V";
+    private final String monstruo = "M";
+    private final String obgeto = "O";
+    
+    
+    
+}
