847b80afcf2343e8403fb86800d3bd606c07ba95
SaulValdunciel <svalduncielg01@educantabria.es>
2026-02-10 16:23:47 +0100

    Update del valientes

--- JavaLand/src/main/java/tema6/javaland/Valientes.java
+++ JavaLand/src/main/java/tema6/javaland/Valientes.java
@@ -8,30 +8,39 @@ package tema6.javaland;
  *
  * @author cuent
  */
-public class Valientes implements PersonajesInterface {
+public class Valientes extends GestorValientes implements PersonajesInterface {
 
     //Atributos
     private int cantidad;
     //Constructores
-    
+    public Valientes(){
+        
+    }
     //Metodos:
     @Override
     public <T> void atacar(T Personaje) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+    //Metodo recibir daño sobrescrito de la interfaz
     @Override
     public int recibirDaño(int cantidad) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        vida=vida-cantidad; //Resta la vida según la cantidad de daño recibida
+        return vida; //Devuelve la vida 
     }
 
     @Override
     public boolean ValienteUsarHabilidadEspecial() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
     }
 
+    //Método para subir de nivel al valiente
     @Override
     public int ValienteSubirNivel() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        vida=vida+10; //Aumenta la vida
+        fuerza=fuerza+1; //Aumenta la fuerza
+        defensa=defensa+1; //Aumenta la defensa
+        habilidad=habilidad+1; //Aumenta la habilidad
+        velocidad=velocidad+1; //Aumenta la velocidad
     }
 }
