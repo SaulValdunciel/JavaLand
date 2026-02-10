cedad5f443747591a6703dd23f0166afbdceb000
cicIESDAM <iqbalkiran808@gmail.com>
2026-02-10 19:27:58 +0100

    upt version

--- JavaLand/src/main/java/tema6/javaland/GestorValientes.java
+++ JavaLand/src/main/java/tema6/javaland/GestorValientes.java
@@ -3,38 +3,44 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package tema6.javaland;
-import java.util.Scanner;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author cuent
  */
+// Esta clase controla los valientes del juego
 public class GestorValientes implements GestoresInterface {
-    //array para guardar nombres de valientes
-    private String[] valientes = new String[4];      
-      
-      //constructor
-      public GestorValientes(){
-          
-      }
-      
+
+    // Aquí guardo los valientes creados
+    private ArrayList<Valiente> lista = new ArrayList<>();
+
+    // Constructor vacío
+    public GestorValientes() {}
+
+    // Método por la interfaz
     @Override
     public void crear() {
-         crearValientes();
+        crearValientes();
     }
-    
+
     // Crear los 4 valientes iniciales 
     //cada uno tiene 40 puntos repartidos entre fuerza, defensa, habilidad, velocidad.
-    private void crearValientes() {
+    public ArrayList<Valiente> crearValientes() {
             // Guerrero → destaca fuerza
-            valientes[0] = "Guerrero (15, 10, 7, 8)";
+            lista.add(new Valiente("Guerrero", 15, 10, 7, 8));
+            
             // Paladín → destaca defensa
-            valientes[1] = "defensa (8, 15, 7, 10)";
+            lista.add(new Valiente("Paladín", 8, 15, 7, 10));
+            
             // Mago → destaca habilidad
-            valientes[2] = "habilidad (7, 8, 17, 8)";
+            lista.add(new Valiente("Mago", 7, 8, 17, 8));
+            
             // Pícaro → destaca velocidad
-            valientes[3] = "Picaro (8, 7, 10, 15)";
+            lista.add(new Valiente("Pícaro", 8, 7, 10, 15));
             
         System.out.println("Valientes creados correctamente.");
-    }        
+        return lista;
+    }
 }
