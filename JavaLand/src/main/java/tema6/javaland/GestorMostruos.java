e3c79914052b5407d3bc60183154bbb21a1437f5
Kiran Iqbal <iqbalkiran808@gmail.com>
2026-02-11 19:11:35 +0100

    save my changes

--- JavaLand/src/main/java/tema6/javaland/GestorMostruos.java
+++ JavaLand/src/main/java/tema6/javaland/GestorMostruos.java
@@ -3,39 +3,46 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package tema6.javaland;
-import java.util.ArrayList;
-import java.util.Random;
+
+import java.util.Scanner;
+
 /**
  *
  * @author cuent
  */
-public class GestorMostruos {
-       
-    ArrayList arrayList = new ArrayList(); 
-
-    //Constructor 
-    public GestorMostruos(){  
+public class GestorMostruos implements GestoresInterface {
+    //array para guarfar niveles de monstruos
+    private int[] monstruo = new int[10];  //maximo 10
+    private int contador = 0;
+    
+    //Constructor vacio
+    public GestorMostruos(){ 
+        
     }
+    @Override
+    public void crear() {
+        crearMostruos();
+    }
+     
     // monstruo con estadisticas segun el nivel (lo año de lista)
-    public Monstruo GenerarMonstruo(int nivel){
-        int vida = 40 + nivel + nivel *10;
-        int fuerza = 10 + nivel;
-        int defensa = 10 + nivel;
-        int habilidad = 10 + nivel;
-        int velocidad = 10 + nivel;
-        
-        //crear un monstruo
-        Monstruo m = new Monstruo("Monstruo nivel" + nivel, vida, fuerza, defensa, habilidad, velocidad);
-        
-        // lista
-        list.add(m);
-        
-        System.out.println("Generado: " + m.getNombre());
-        return m;
-    }
-   // eliminar monstruo
+    public void GenerarMonstruo(int nivel){
+        if (contador < monstruo.length){
+            monstruo[contador] = nivel;
+            System.out.println("Monstruo creado de nivel: " + nivel);
+        } else{
+            System.out.println("No se pude crear más monstruos.");
+        }
+    } 
+    //Eliminar el ultimo monstruo creado
     public void eliminarMonstruo(){
-        list.remove();
+        if (contador > 0) {
+            contador--;
+            System.out.println("Monstruo eliminado.");
+        } else {
+            System.out.println("No hay monstruos para eliminar");
+        }
     }
-        
+
+    private void crearMostruos() {
+    }
 }
--- JavaLand/src/main/java/tema6/javaland/GestoresInterface.java
+++ JavaLand/src/main/java/tema6/javaland/GestoresInterface.java
@@ -8,14 +8,6 @@ package tema6.javaland;
  *
  * @author cuent
  */
-/**
- * Interfaz común para los gestores del proyecto Javaland.
- * Obliga a implementar un método crear().
- */
 public interface GestoresInterface {
-
-    /**
-     * Método genérico de creación.
-     */
     void crear();
 }
