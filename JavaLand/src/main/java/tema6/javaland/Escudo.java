d75feb4fbad1ce86af7dd2011cdcdd1e8066d969
Kiran Iqbal <iqbalkiran808@gmail.com>
2026-02-11 20:05:38 +0100

    UPDATES CLASS ARMA Y ESCUDO

--- JavaLand/src/main/java/tema6/javaland/Escudo.java
+++ JavaLand/src/main/java/tema6/javaland/Escudo.java
@@ -8,6 +8,22 @@ package tema6.javaland;
  *
  * @author DAM102
  */
-public class Escudo {
+public class Escudo extends Objetos {
+    // atributos: nombre y defensa
+    private String nombre;
+    private int defensa;
     
+    //constructor 
+    public Escudo(String nombre,int defensa){
+        
+    }
+    //metodos: seter
+    //setNombre
+    public void setNombre(){
+        
+    }
+    //setDefensa
+    public void setDefensa(){
+        
+    }
 }
